4d0001da56a68fd7f4e3a4754bdf1521aac0a307
Carlos Quim <128547662+CarlosQuim2019172@users.noreply.github.com>
2023-04-13 11:52:24 -0600

    Update Luces_Autofanastico.ino

--- Luces_Autofanastico/Luces_Autofanastico.ino
+++ Luces_Autofanastico/Luces_Autofanastico.ino
@@ -8,30 +8,13 @@ Curso: Taller de Electrónica Digital y Reparación de Computadoras I
 Nombre: Carlos Alexander Quim Chapaz
 Carné: 2019172 
 */
-#define L1 2
-#define L2 3
-#define L3 4
-#define L4 5
-#define L5 6
-#define L6 7
-#define L7 8
-#define L8 9
-#define L9 10
-#define L10 11
 #define PMO(pin) pinMode(pin,OUTPUT)
 int i;
 
 void setup() {
-  PMO(2);
-  PMO(3);
-  PMO(4);
-  PMO(5);
-  PMO(6);
-  PMO(7);
-  PMO(8);
-  PMO(9);
-  PMO(10);
-  PMO(11);
+for(i=2;i<11;i++){
+  PMO(i);
+}
 }
 
 void loop() {
