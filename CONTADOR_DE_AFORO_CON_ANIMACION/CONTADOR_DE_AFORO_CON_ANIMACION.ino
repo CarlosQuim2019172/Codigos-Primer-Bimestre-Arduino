fc212f31a5ed1018c929058e0a4710c3ed2a9cc0
Carlos Quim <128547662+CarlosQuim2019172@users.noreply.github.com>
2023-04-28 11:48:02 -0600

    Update CONTADOR_DE_AFORO_CON_ANIMACION.ino

--- CONTADOR_DE_AFORO_CON_ANIMACION/CONTADOR_DE_AFORO_CON_ANIMACION.ino
+++ CONTADOR_DE_AFORO_CON_ANIMACION/CONTADOR_DE_AFORO_CON_ANIMACION.ino
@@ -13,12 +13,10 @@ Carné: 2019172 
 #define ECHO 4                                                  
 #define BUZ 3
 
-int gente = 0; //número de personas
-long d; //Variable de distancia en cm
-
 int medicion_Ultrasonico();
-int aforo();
-int contador(int valor);
+void aforo();
+
+int personas, gente;
 
 LiquidCrystal lcd_quim(5,6,7,8,9,10);
 
@@ -44,11 +42,35 @@ byte customChar1[] = {
   B10001
 };
 
+byte explosion[] = {
+  B00100,
+  B10101,
+  B01110,
+  B11111,
+  B01110,
+  B10101,
+  B00100,
+  B00000
+};
+
+byte barra[] = {
+  B00100,
+  B01110,
+  B10101,
+  B01110,
+  B10101,
+  B01110,
+  B10101,
+  B00100
+};
+
 void setup() {
   Serial.begin(9600);
   lcd_quim.begin(16,2);
   lcd_quim.createChar(0, customChar);
   lcd_quim.createChar(1, customChar1);
+  lcd_quim.createChar(2, explosion);
+  lcd_quim.createChar(3, barra);
   lcd_quim.home();
   pinMode(BUZ, OUTPUT);
   pinMode(TRIG, OUTPUT);
@@ -57,12 +79,11 @@ void setup() {
 }
 
 void loop() {
-  medicion_Ultrasonico();
   aforo();
-  contador(gente);
 }
 
 int medicion_Ultrasonico(){
+  long d; //Variable de distancia en cm
   long t; //Variable de tiempo en llegar al ECHO
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10); //Pulso de 10us
@@ -73,31 +94,62 @@ int medicion_Ultrasonico(){
   return d;
 }
 
-int aforo(){
-  if(d >= 6 && d <=10){
-    if(gente < 16){
+void aforo(){  
+  personas = medicion_Ultrasonico();
+
+  if(personas >= 6 && personas <=10){
     gente = gente + 1;
-    }
     delay(500);
     Serial.println(gente);
   }
-  return gente;  
-}
-
-int contador(int valor){
-  if(valor <=16){
+  
+  if(gente <=16){
   lcd_quim.setCursor(0,0);
   lcd_quim.print("Aforo:");
   lcd_quim.setCursor(7,0);
-  lcd_quim.print(valor);
+  lcd_quim.print(gente);
   }
   
-  if(valor == 16){
+  if(gente >= 16){
+  lcd_quim.clear();
+  lcd_quim.setCursor(0,0);
+  lcd_quim.print("    El AFORO    ");
+  lcd_quim.setCursor(0,1);
+  lcd_quim.print("    COMPLETO    ");
   lcd_quim.setCursor(0,2);
+  // personas con brazos abajo
+  lcd_quim.write(byte(0));
+  lcd_quim.setCursor(15,2);
   lcd_quim.write(byte(0));
   delay(500);
+  // personas con brazos arriba
   lcd_quim.setCursor(0,2);
   lcd_quim.write(byte(1));
+  lcd_quim.setCursor(15,2);
+  lcd_quim.write(byte(1));
+
+  // barras
+  lcd_quim.setCursor(13,1);
+  lcd_quim.write(byte(3));
+  lcd_quim.setCursor(2,1);
+  lcd_quim.write(byte(3));
+  delay(500);  
+  lcd_quim.setCursor(13,0);
+  lcd_quim.write(byte(3));
+  lcd_quim.setCursor(2,0);
+  lcd_quim.write(byte(3));
+  
+  delay(500);
+  // explocion
+  lcd_quim.setCursor(13,0);
+  lcd_quim.write(byte(2));
+  lcd_quim.setCursor(13,1);
+  lcd_quim.print(" ");
+  lcd_quim.setCursor(2,0);
+  lcd_quim.write(byte(2));
+  lcd_quim.setCursor(2,1);
+  lcd_quim.print(" ");
+  
   tone(BUZ,260);
   delay(500);
   noTone(BUZ);
