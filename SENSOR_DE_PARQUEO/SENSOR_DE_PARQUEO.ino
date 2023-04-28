d0e19a1154a65e912aa037137b98dc6c47b4c1fe
Carlos Quim <128547662+CarlosQuim2019172@users.noreply.github.com>
2023-04-27 21:51:13 -0600

    Create SENSOR_DE_PARQUEO.ino

---
+++ SENSOR_DE_PARQUEO/SENSOR_DE_PARQUEO.ino
@@ -0,0 +1,127 @@
+/*
+Fundación Kinal
+Centro Educativo Técnico Labral Kinal 
+Electrónica
+Grado: Quinto
+Sección: A
+Curso: Taller de Electrónica Digital y Reparación de Computadoras I
+Nombre: Carlos Alexander Quim Chapaz
+Carné: 2019172 
+*/
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#define TRIG 2
+#define ECHO 3 
+
+long d; //Variable de distancia en cm
+int i,a;
+int medicion_Ultrasonico();
+int distancia(int valor);
+
+LiquidCrystal_I2C lcd_quim(0x27,16,2); 
+
+byte customChar[] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+};
+
+void setup() {
+  Serial.begin(9600);
+  lcd_quim.init();  
+  lcd_quim.backlight();
+  lcd_quim.createChar(0, customChar);
+  lcd_quim.home();
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT); //PIN de Entrada
+  digitalWrite(TRIG, LOW); //Incializamos el TRIGGER en 0 
+
+}
+
+void loop() {
+   medicion_Ultrasonico();
+   distancia(d);
+}
+
+int medicion_Ultrasonico(){
+  long t; //Variable de tiempo en llegar al ECHO
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10); //Pulso de 10us
+  digitalWrite(TRIG, LOW);
+  
+  t = pulseIn(ECHO, HIGH); //obtenemos el ancho del pulso
+  d = t/58; //convertimos el tiempo en una distancia en cm
+  Serial.println(d);
+  return d;
+}
+
+int distancia(int valor){
+  if(valor > 70){
+    lcd_quim.clear();
+  }
+  switch(valor){
+    case 50:
+      lcd_quim.setCursor(7,0);
+      lcd_quim.print("50    ");
+      lcd_quim.setCursor(0,1);
+      lcd_quim.print("     ");
+      lcd_quim.setCursor(5,1);
+      lcd_quim.print("LIBRES  ");
+      i = 0;
+      a = 13;
+      for(i == 0; i <=2; i++){
+        lcd_quim.setCursor(i,0);
+        lcd_quim.write(byte(0));
+        lcd_quim.setCursor(3,0);
+        lcd_quim.print("    ");
+      }
+      for(a == 13; a <=15; a++){
+        lcd_quim.setCursor(a,0);
+        lcd_quim.write(byte(0));
+      }
+    break;
+
+    case 30:
+      lcd_quim.setCursor(7,0);
+      lcd_quim.print("30  ");
+      lcd_quim.setCursor(4,1);
+      lcd_quim.print("Cuidado!");
+      i = 0;
+      a = 11;
+      for(i == 0; i <=4; i++){
+        lcd_quim.setCursor(i,0);
+        lcd_quim.write(byte(0));
+        lcd_quim.setCursor(5,0);
+        lcd_quim.print("  ");
+      }
+      for(a == 11; a <=15; a++){
+        lcd_quim.setCursor(a,0);
+        lcd_quim.write(byte(0));
+      }
+    break;
+
+    case 10:
+      lcd_quim.setCursor(7,0);
+      lcd_quim.print("10");
+      lcd_quim.setCursor(0,1);
+      lcd_quim.print("     ");
+      lcd_quim.setCursor(5,1);
+      lcd_quim.print("Alto!! ");
+      i = 0;
+      a = 9;
+      for(i == 0; i <=6; i++){
+        lcd_quim.setCursor(i,0);
+        lcd_quim.write(byte(0));
+      }
+      for(a == 9; a <=15; a++){
+        lcd_quim.setCursor(a,0);
+        lcd_quim.write(byte(0));
+      }
+    break;
+  }
+}
