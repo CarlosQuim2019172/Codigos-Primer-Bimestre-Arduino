e6f4a10d5bb34267395148c5289beaa8e0f84f46
Carlos Quim <128547662+CarlosQuim2019172@users.noreply.github.com>
2023-05-01 23:29:14 -0600

    Update TERMOMETRO_CON_LCD.ino

--- TERMOMETRO_CON_LCD/TERMOMETRO_CON_LCD.ino
+++ TERMOMETRO_CON_LCD/TERMOMETRO_CON_LCD.ino
@@ -1,3 +1,14 @@
+/*
+Fundación Kinal
+Centro Educativo Técnico Labral Kinal 
+Electrónica
+Grado: Quinto
+Sección: A
+Curso: Taller de Electrónica Digital y Reparación de Computadoras I
+Nombre: Carlos Alexander Quim Chapaz
+Carné: 2019172 
+*/
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
@@ -9,17 +20,78 @@ LiquidCrystal_I2C lcd_quim(0x27,16,2);
 OneWire ourWire(2);
 DallasTemperature sensor(&ourWire);
 
-float sensortF();
-float sensortC();
+int sensortF();
+int sensortC();
 void medicion();
 
-float TempF, TempC;
+int TempF, TempC;
+
+byte termometro[] = {
+  B01110,
+  B01010,
+  B01010,
+  B01010,
+  B10001,
+  B10001,
+  B10001,
+  B01110
+};
+
+byte caliente[] = {
+  B00000,
+  B00100,
+  B00110,
+  B01110,
+  B11111,
+  B11111,
+  B11111,
+  B01110
+};
+
+byte frio[] = {
+  B00100,
+  B10101,
+  B01110,
+  B11111,
+  B01110,
+  B10101,
+  B00100,
+  B00000
+};
+
+byte templado[] = {
+  B11011,
+  B00000,
+  B01010,
+  B01010,
+  B00000,
+  B10001,
+  B01110,
+  B00000
+};
+
+byte grados[] = {
+  B11100,
+  B10100,
+  B11100,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
 
 void setup() {
   Serial.begin(9600);   //Inicio la comunicacion serial
   lcd_quim.init();  
   lcd_quim.backlight();
   sensor.begin();   //Se inicia el sensor
+  lcd_quim.createChar(0, termometro);
+  lcd_quim.createChar(1, caliente);
+  lcd_quim.createChar(2, frio); 
+  lcd_quim.createChar(3, templado);
+  lcd_quim.createChar(4, grados); 
+  lcd_quim.home();
   pinMode(LEDR,OUTPUT);
   pinMode(LEDA,OUTPUT);
   pinMode(LEDB,OUTPUT);
@@ -29,55 +101,66 @@ void loop() {
   medicion();
 }
 
-float sensortC(){
+int sensortC(){
   sensor.requestTemperatures();   //Se envía el comando para leer la temperatura
-  float temp= sensor.getTempCByIndex(0); //Se obtiene la temperatura en ºC
+  int temp= sensor.getTempCByIndex(0); //Se obtiene la temperatura en ºC
   return temp;
 }
 
-float sensortF(){
+int sensortF(){
   sensor.requestTemperatures();   //Se envía el comando para leer la temperatura
-  float temp1 = sensor.getTempFByIndex(0); //Se obtiene la temperatura en ºF
+  int temp1 = sensor.getTempFByIndex(0); //Se obtiene la temperatura en ºF
   return temp1;
 }
 
 void medicion(){
   TempF = sensortF();
   TempC = sensortC();
+  
+  
+  lcd_quim.setCursor(0,0);
+  lcd_quim.print("  TERMOMETRO ");
+  lcd_quim.write(byte(0));
+  //Temperatura en Celsius
+  lcd_quim.setCursor(2,1);
+  lcd_quim.print(TempC);
+  lcd_quim.write(byte(4));
+  lcd_quim.print("C");
+  //Temperatura en Fahrenheit
+  lcd_quim.setCursor(8,1);
+  lcd_quim.print(TempF);
+  lcd_quim.write(byte(4));
+  lcd_quim.print("F "); 
 
-  if(TempC > 15 && TempC < 30){
+
+  if(TempC >= 15 && TempC <= 30){
+    lcd_quim.setCursor(13,1);
+    lcd_quim.write(byte(3));
+    delay(800);
+    lcd_quim.setCursor(13,1);
+    lcd_quim.print(" ");
     digitalWrite(LEDR,LOW);
     digitalWrite(LEDB,LOW);
     digitalWrite(LEDA,HIGH);
   }
-  
-  //Temperatura en Celsius
-  lcd_quim.setCursor(0,0);
-  lcd_quim.print("Temp = ");
-  lcd_quim.setCursor(7,0);
-  lcd_quim.print(TempC);
-  lcd_quim.setCursor(13,0);
-  lcd_quim.print((char)223);
-  lcd_quim.setCursor(14,0);
-  lcd_quim.print("C");
-
-  //Temperatura en Fahrenheit
-  lcd_quim.setCursor(0,1);
-  lcd_quim.print("Temp = ");
-  lcd_quim.setCursor(7,1);
-  lcd_quim.print(TempF);
-  lcd_quim.setCursor(13,1);
-  lcd_quim.print((char)223);
-  lcd_quim.setCursor(14,1);
-  lcd_quim.print("F");  
 
   if(TempC > 30){
+    lcd_quim.setCursor(13,1);
+    lcd_quim.write(byte(1));
+    delay(800);
+    lcd_quim.setCursor(13,1);
+    lcd_quim.print(" ");
     digitalWrite(LEDR,HIGH);
     digitalWrite(LEDA,LOW);
     digitalWrite(LEDB,LOW);
   }
 
   if(TempC < 15){
+    lcd_quim.setCursor(13,1);
+    lcd_quim.write(byte(2));
+    delay(800);
+    lcd_quim.setCursor(13,1);
+    lcd_quim.print(" ");
     digitalWrite(LEDR,LOW);
     digitalWrite(LEDA,LOW);
     digitalWrite(LEDB,HIGH);
