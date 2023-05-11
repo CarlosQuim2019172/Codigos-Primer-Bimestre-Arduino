d2af8671fe06341b8a86445bcf2159ac59801b97
Carlos Quim <128547662+CarlosQuim2019172@users.noreply.github.com>
2023-05-11 15:40:15 -0600

    Update Proyecto_Final_No.4___Secuencia_de_luces.ino

--- Proyecto_Final_No.4___Secuencia_de_luces/Proyecto_Final_No.4___Secuencia_de_luces.ino
+++ Proyecto_Final_No.4___Secuencia_de_luces/Proyecto_Final_No.4___Secuencia_de_luces.ino
@@ -10,19 +10,38 @@ Carné: 2019172 
 */
 #include <Adafruit_NeoPixel.h>
 #include <LiquidCrystal_I2C.h>
-#define NUMPIXELS 14 // definimos la cantidad da leds del NEOPIXEL
-#define NEO 2
-#define BUZ 3
+#include <Keypad.h>
+#define NUMPIXELS 15 // definimos la cantidad da leds del NEOPIXEL
+#define NEO 14
+#define BUZ 10
 
+const byte FILAS = 4;
+const byte COLUMNAS = 4;
+
+char keys[FILAS][COLUMNAS]={
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte pinesFilas[FILAS] = {9,8,7,6};
+byte pinesColumnas[COLUMNAS] = {5,4,3,2};
+
+Keypad teclado = Keypad(makeKeymap(keys),pinesFilas,pinesColumnas,FILAS,COLUMNAS);
 LiquidCrystal_I2C lcd_proyecto(0x27,16,2);
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEO, NEO_GRB + NEO_KHZ800);
 
+void secuenciasluces();
 void nuestrasecuencia();
-int pinespixels();
-int cantidad;
+int colorespixels();
+int i; //colores a utilizar
+char tecla;
+
 
 void setup() {
   Serial.begin(9600);
+  pixels.clear(); // Apaga todos los NeoPixels
   lcd_proyecto.init();  
   lcd_proyecto.backlight();
   pinMode(NEO, OUTPUT);
@@ -30,23 +49,34 @@ void setup() {
 }
 
 void loop() {
-  nuestrasecuencia();
-
+  tecla = teclado.getKey();
+  secuenciasluces();
+  colorespixels();
 }
 
-void nuestrasecuencia(){
-  cantidad = pinespixels();
-
-  Serial.println(cantidad);
-    
+void secuenciasluces(){
+  if(tecla == 'A'){
+    nuestrasecuencia();
+  }
 }
-
-int pinespixels(){
-  int npixel;
   
-     for(int i =0; i<15; i++){ 
-      npixel = i; 
-   }
 
-  return npixel;
+void nuestrasecuencia(){ // funcionde la quinta secuencia 
+  int color = colorespixels();
+  Serial.println(tecla);
+    for(i = 0; i < NUMPIXELS; i++){
+      pixels.setPixelColor(i,color);
+      pixels.show();
+    }  
+    delay(500);
+    for(i = 0; i < NUMPIXELS; i++){
+      pixels.setPixelColor(i,0,0,0);
+      pixels.show();  
+    }
+    delay(500);    
+    for(i = NUMPIXELS; i > 0; i++){
+      pixels.setPixelColor(i,color);
+      pixels.show();
+    }
+  
 }
